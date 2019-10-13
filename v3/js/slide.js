309eaf3da9cfeed6f4e67a2bbcfa6ee8976bcf0d
Poting Chen <czerone1011@gmail.com>
2019-10-14 00:13:00 +0800

    add timer to v3

--- v3/js/slide.js
+++ v3/js/slide.js
@@ -1,16 +1,17 @@
 $(function() {
+    var time = 0;
+    var pause=true;
+    var set_timer;
     // 位置轉座標的換算表
     var posConv = {};
     for(var i=0;i<9;i++) {
-        $("#dvPuzzle").append("<div class='PicCell' id='Pic"+i+"'><img src='img/dragon960.png' /></div>");
+        $("#dvPuzzle").append("<div class='PicCell' id='Pic" + i + "'><img src='img/dragon960.png' /></div>");
         var row = parseInt(i / 3);
         var col = i % 3;
-        $("#Pic"+i+" img").css("margin-left", col * -320 + 1).css("margin-top", row * -240 + 1);
+        $("#Pic" + i + " img").css("margin-left", col * -320 + 1).css("margin-top", row * -240 + 1);
         // 第i個轉成第row列第col行
         posConv[i] = {row:row, col:col};
     }
-    // 除去左上角
-    $("#Pic0 img").remove();
     // 取得相鄰位置
     function getNearPos(i) {
         var pool = [];
@@ -35,10 +36,10 @@ $(function() {
         var toCheck = getNearPos(i);
         while(toCheck.length > 0){
             var j = toCheck.pop();
-            if(cells.eq(j).attr("id")=="Pic0") { // 為空白格,交換位置
-                // 排序,必要時對調,讓i<j
+            if(cells.eq(j).attr("id") == "Pic0") { // 為空白格,交換位置
+                // 排序,必要時對調,讓i < j
                 if(i > j){
-                    var k=j;j=i;i=k;
+                    var k = j;j = i;i = k;
                 }
                 var ahead = cells.eq(i);
                 var behind = cells.eq(j);
@@ -53,7 +54,27 @@ $(function() {
             }
         }
     });
+    function timer() { //定時函式，每一秒執行一次
+        time += 1; //一秒鐘加一，單位是秒
+        var min = parseInt(time/60); //把秒轉換為分鐘，一分鐘60秒，取商就是分鐘
+        var sec = time%60; //取餘就是秒
+        $("#timer").text(min + "分" + sec + "秒"); //然後把時間更新顯示出來
+    }
+    $("#start").click(function() { //開始暫停函式
+        if(pause) {
+            $("#start").text("暫停"); //把按鈕文字設定為暫停
+            pause = false; //暫停表示設定為false
+            set_timer = setInterval(timer, 1000); //啟動定時
+        } //如果當前是暫停，則開始
+        else {
+            $("#start").text("開始");
+            pause = true;
+            clearInterval(set_timer);
+        }
+    });
     $("#reset").click(function() {
+        // 除去左上角
+        $("#Pic0 img").remove();
         for(var i=0;i<500;i++) {
             var cells = $("#dvPuzzle div");
             // 找出空格所在位置,並取得其相鄰圖塊
